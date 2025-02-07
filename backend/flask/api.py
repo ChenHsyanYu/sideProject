8d6509c954331ffd50aea95a2e2bdb532d41578a
ChenHsuanYu <sharonchen82228@gmail.com>
2025-02-07 17:04:37 +0800

    測試資料庫連線

--- backend/flask/api.py
+++ backend/flask/api.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
 # from db import allProjects  # 匯入 MongoDB 連接
+import os
+import sys
+from database.db import cursor
 
 app = Flask(__name__)
 
@@ -10,7 +13,10 @@ def home():
 
 @app.route("/project", methods=["GET"])
 def project():
-    return "get all projects"
+    cursor.execute("SELECT * FROM user")
+    users = cursor.fetchall()
+    cursor.close()
+    return jsonify(users)
 
 
 if __name__ == "__main__":
